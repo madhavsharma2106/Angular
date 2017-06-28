dab37f8f88c5a7ce1f997b88821d313822874de0
SOR-INFY <madhav.sharma@idemo-ppc.com>
2017-06-28 11:31:57 +0530

    Twtitter App added

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,4 @@ app.post('/search', functions.search);
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, "10.67.225.109", () => {
-    console.log("App Started on 3000 ");
-});
+app.listen(port);
