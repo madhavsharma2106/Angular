bd3e0b62f1fea0b8fa6c22d82735151effdf0173
Madhav Sharma <madhavsharma2106@gmail.com>
2017-07-17 21:13:07 +0530

    Customized charts with options

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -10,8 +10,22 @@ export class ChartsComponent implements OnInit {
 
   // Doughnut
   public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public bgColor: any = [
+    {
+      backgroundColor: [
+        'rgba(29, 83, 150, 1)',
+        'rgba(204, 59, 59, 1)',
+        'rgba(136, 196, 37, 1)'
+      ]
+    }
+  ];
   public doughnutChartData: number[] = [350, 450, 100];
   public doughnutChartType: string = 'doughnut';
+  public options = {
+    cutoutPercentage: 90,
+  };
+
+  
 
   ngOnInit() {
   }
