2b776cb9f506771c5708151beacfa04aa25fb997
Madhav Sharma <madhavsharma2106@gmail.com>
2017-07-19 20:44:04 +0530

    Calendar date modified

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -10,21 +10,15 @@ export class CalendarComponent implements OnInit {
   constructor() { }
 
   today = new Date();
-  currentDate = this.today.getDate();
-  // currentMonth = this.today.getMonth() + 1;
-  currentMonth = 8;
-  currentYear = this.today.getFullYear();
-  nextMonth = this.currentMonth + 1;
-  previousMonth = this.currentMonth - 1;
-
-  currentMonthTotalDays = new Date(this.currentYear, this.currentMonth, 0).getDate();
-  PrevMonthTotalDays = new Date(this.previousMonth, this.previousMonth, 0).getDate();
-  startDay = new Date(this.currentYear + "-" + this.currentMonth + "-01").getDay();
-  PrevMonthStartDate = this.PrevMonthTotalDays - this.startDay + 1;
-  nextMonthEndDate = 42 - (42 - (7 - (7 - this.startDay))) - 1;
+  currentMonth = this.today;
 
   ngOnInit() {
-    console.log(42 - (42 - (7 - (7 - this.startDay))) - 1);
+   
+  }
+
+  nextMonth(){
+    var test = this.today.setMonth(this.today.getMonth()+1);
+    this.currentMonth = new Date(test);
 
   }
 
